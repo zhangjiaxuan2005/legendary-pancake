da81420cb97ffdeb060f13127ae729b62262b3b5
zhangjiaxuan2005 <1724640501@qq.com>
2025-07-14 22:19:05 +0800

    feat(Base_class): 添加 fstream_one 可执行文件
    - 在 CMakeLists.txt 中添加 fstream_one 的构建规则
    - 在 build.ninja 中添加 fstream_one 的构建和链接命令
    - 在 TargetDirectories.txt 中添加 fstream_one 的目标目录
    - 在 fstream_one.cpp 中实现文件写入的简单示例

--- cpp/Base_class/Day_tan/fstream_one.cpp
+++ cpp/Base_class/Day_tan/fstream_one.cpp
@@ -2,3 +2,13 @@
 // Created by 17246 on 2025/7/14.
 //
 #include <fstream>
+#include <iostream>
+using namespace std;
+int main()
+{
+    ofstream ofs;
+    ofs.open("test.txt",ios::out);
+    ofs<<"hello world"<<endl;
+    ofs.close();
+    return 0;
+}
